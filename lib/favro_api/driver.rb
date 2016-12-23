cbf0cfc62a6d72ba24cfa7f4018192911cf2bb14
Sergey Besedin <kr3ssh@gmail.com>
2016-12-23 12:55:10 +0300

    Refactor favro driver

--- lib/favro_api/driver.rb
+++ lib/favro_api/driver.rb
@@ -4,51 +4,62 @@ require_relative './data_types'
 module FavroApi
   class Driver
     def cards(params = {})
-      response = Request.fetch(:cards, params)
+      response = paginated_request(:cards, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Card)
     end
 
     def collections(params = {})
-      response = Request.fetch(:collections, params)
+      response = paginated_request(:collections, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Collection)
     end
 
     def comments(params = {})
-      response = Request.fetch(:comments, params)
+      response = paginated_request(:comments, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Comment)
     end
 
     def custom_fields(params = {})
-      response = Request.fetch(:custom_fields, params)
+      response = paginated_request(:custom_fields, params).fetch
 
       response.parse
     end
 
     def organizations(params = {})
-      response = Request.fetch(:organizations, params)
+      response = paginated_request(:organizations, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Organization)
     end
 
     def tasks(params = {})
-      response = Request.fetch(:tasks, params)
+      response = paginated_request(:tasks, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Task)
     end
 
     def tasklists(params = {})
-      response = Request.fetch(:tasklists, params)
+      response = paginated_request(:tasklists, params).fetch
 
       response.parse(type: FavroApi::DataTypes::Tasklist)
     end
 
     def users(params = {})
-      response = Request.fetch(:users, params)
+      response = paginated_request(:users, params).fetch
 
       response.parse(type: FavroApi::DataTypes::User)
+    end
+
+    private
+
+    def paginated_request(endpoint, params)
+      Request.new(
+        endpoint: endpoint,
+        page: params.delete(:page),
+        last_response: params.delete(:last_response),
+        params: params
+      )
     end
   end
 end
--- lib/favro_api/request.rb
+++ lib/favro_api/request.rb
@@ -16,21 +16,27 @@ module FavroApi
       users:          '/users'
     }.freeze
 
-    attr_accessor :connection, :url, :params, :method
+    attr_accessor :connection, :url, :endpoint, :params, :method,
+      :last_response, :page
 
     def initialize(options = {})
-      self.url    = options[:url]
-      self.method = options[:method]
-      self.params = options[:params]
-
-      self.connection = Faraday.new("#{uri.scheme}://#{uri.hostname}") do |faraday|
-        faraday.adapter :net_http
-        faraday.headers['organizationId'] = params.delete(:organization_id)
-        faraday.basic_auth(FavroApi.auth.email, FavroApi.auth.token)
-      end
+      self.url           = options[:url]
+      self.endpoint      = options[:endpoint]
+      self.method        = options[:method] || :get
+      self.params        = options[:params] || {}
+      self.last_response = options[:last_response]
+      self.page          = options[:page]
     end
 
     def fetch
+      self.connection = Faraday.new("#{uri.scheme}://#{uri.hostname}") do |faraday|
+        faraday.adapter :net_http
+        faraday.basic_auth(FavroApi.auth.email, FavroApi.auth.token)
+        faraday.headers['organizationId'] = params.delete(:organization_id)
+        faraday.params['page']      = page || (last_response ? last_response.page + 1 : 0)
+        faraday.params['requestId'] = last_response&.request_id
+      end
+
       response = Response.new(response: connection.send(method, uri.request_uri, params))
 
       if response.error?
@@ -41,19 +47,27 @@ module FavroApi
     end
 
     class << self
-      def fetch(endpoint, params = {})
-        new(url: api_url_for(endpoint), method: :get, params: params).fetch
-      end
-
-      private
-
-      def api_url_for(endpoint)
-        path = ENDPOINTS[endpoint] || raise(ApiError, "Unknown endpoint #{endpoint}")
-        "#{API_URL}#{path}"
+      def fetch(endpoint, page: nil, last_response: nil, params: {})
+        new(
+          endpoint:      endpoint,
+          last_response: last_response,
+          page:          page,
+          method:        :get,
+          params:        params
+        ).fetch
       end
     end
 
+    def url
+      @url ||= api_url_for(endpoint)
+    end
+
     private
+
+    def api_url_for(endpoint)
+      path = ENDPOINTS[endpoint] || raise(ApiError, "Unknown endpoint #{endpoint}")
+      "#{API_URL}#{path}"
+    end
 
     def uri
       URI(url)
